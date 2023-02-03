5b2a14c0669decb92d9ac1256438282792ad50ae
Neto Almeida <almeidaneto1408@gmail.com>
2023-02-03 20:02:19 -0300

    ok

---
+++ ex005.js
@@ -0,0 +1,8 @@
+
+var vel = 60.1
+
+if (vel > 60) {
+    console.log(`Sua velocidade é ${vel}Km/h. Você está acima do permitido: MULTADO!`)
+} else {
+    console.log('Parabéns, você está na velocidade da via!')
+}
