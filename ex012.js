4074735a7f7a82dfc49abb004d7366de378490b1
Neto Almeida <almeidaneto1408@gmail.com>
2023-02-08 18:19:57 -0300

    Create ex012.js

---
+++ ex012.js
@@ -0,0 +1,12 @@
+
+const num = [2,1,4,3]
+
+num.sort()
+num.push(9)
+
+console.log(`O array tem ${num.length} numeros`)
+console.log(`Os números são: ${num}`)
+
+for (var p = 0; p < num.length; p++){
+    console.log (num[p])
+}
