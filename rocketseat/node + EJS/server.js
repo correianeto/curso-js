29b3ebc1c836b0edbb57c8bb4b9988ee4d451cd8
correianeto <almeidaneto1408@gmail.com>
2023-04-08 15:51:29 -0300

    alguns exercicios de ejs

--- rocketseat/node + EJS/server.js
+++ rocketseat/node + EJS/server.js
@@ -5,11 +5,39 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', function (req,res) {
-    res.render('index')
+    const items = [
+        {
+            tittle: 'D',
+            message: 'esenvolver aplicações de forma fácil'
+        },
+        {
+            tittle: 'E',
+            message: 'JS usa JavaScript para renderizar HTML'
+        },
+        {
+            tittle: 'M',
+            message: 'uito fácil de usar'
+        },
+        {
+            tittle: 'A',
+            message: 'morzinho'
+        },
+        {
+            tittle: 'I',
+            message: 'nstall EJS'
+        },
+        {
+            tittle: 'S',
+            message: 'intaxe simples'
+        }
+    ]
+
+    const subtittle = 'Uma linguagem de modelagem para criação de página HTML utilizando JavaScript'
+    res.render('pages/index', {qualities: items, subtittle: subtittle})
 })
 
 app.get('/sobre', function (req,res) {
-    res.render('about')
+    res.render('pages/about')
 })
 
 app.listen(8080)
